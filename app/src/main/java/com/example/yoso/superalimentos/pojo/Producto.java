f52c340767dfd4946711ac57f534826fc0f01e19
Jordi Desumvila <jdrcoursera@gmail.com>
2017-04-19 23:43:57 +0200

    Pojo

---
+++ app/src/main/java/com/example/yoso/superalimentos/pojo/Producto.java
@@ -0,0 +1,65 @@
+package com.example.yoso.superalimentos.pojo;
+
+/**
+ * POJO PRODUCTO
+ * Created by yoso on 19/04/17.
+ */
+
+public class Producto {
+    // Atributos
+    private int id;
+    private String nombre;
+    private int unidades;
+    private int precio;
+    private int idDrawable;
+
+    // Constructor
+    public Producto(String nombre, int unidades, int precio, int idDrawable) {
+        this.nombre = nombre;
+        this.unidades = unidades;
+        this.precio = precio;
+        this.idDrawable = idDrawable;
+    }
+
+
+    // Métodos getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getIdDrawable() {
+        return idDrawable;
+    }
+
+    public void setIdDrawable(int idDrawable) {
+        this.idDrawable = idDrawable;
+    }
+}
