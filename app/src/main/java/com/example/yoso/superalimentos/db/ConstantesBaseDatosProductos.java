5bc02911d54cb5a31fa1483c1d54f125337bcb10
Jordi Desumvila <jdrcoursera@gmail.com>
2017-04-20 23:31:29 +0200

    Constante Base de Datos

---
+++ app/src/main/java/com/example/yoso/superalimentos/db/ConstantesBaseDatosProductos.java
@@ -0,0 +1,19 @@
+package com.example.yoso.superalimentos.db;
+
+/**
+ * Created by yoso on 20/04/17.
+ */
+
+public class ConstantesBaseDatosProductos {
+    // Constantes Base de datos
+    public static final String DATABASE_NAME = "productos";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_PRODUCTO = "producto";
+    public static final String TABLE_PRODUCTO_ID="id";
+    public static final String TABLE_MASCOTA_NOMBRE = "nombre";
+    public static final String TABLE_MASCOTA_UNIDAD = "unidad";
+    public static final String TABLE_MASCOTA_PRECIO = "precio";
+    public static final String TABLE_MASCOTA_FOTO = "foto";
+
+}
